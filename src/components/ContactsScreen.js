495ec10608b3f7c9808e398dadd1ebd94ab1eb27
Liem Pham <pham.ducliem@gmail.com>
2020-01-13 22:39:50 +0000

    Add React Navigation and ContactScreen

---
+++ src/components/ContactsScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ContactsScreen = () => {
+  return (
+    <View style={styles.wrap}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrap: {
+    flex: 1,
+  },
+});
+
+export default ContactsScreen;
